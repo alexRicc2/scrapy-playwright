93d46de03ade6cdb21a52de2b09487839bafadb9
alexRicc2 <alex.65.santanna@gmail.com>
2023-07-05 18:29:21 -0300

    commiting before coing to windows

--- quotes_js_scraper/spiders/shein.py
+++ quotes_js_scraper/spiders/shein.py
@@ -6,12 +6,13 @@ class SheinSpider(scrapy.Spider):
     allowed_domains = ["br.shein.com"]
     
     def start_requests(self):
-        url = 'https://br.shein.com'
+        url = 'https://br.shein.com/SHEIN-MOD-Solid-PU-Leather-Tube-Top-p-12877530-cat-2223.html'
         yield scrapy.Request(url, callback=self.parse ,meta=dict(
             playwright = True,
             playwright_include_page = True,
             playwright_page_methods = [
-                PageMethod('evaluate', 'window.scrollBy(0, document.body.scrollHeight)')
+                PageMethod('evaluate', 'window.scrollBy(0, document.body.scrollHeight)'),
+                PageMethod('wait_for_selector', "div.common-reviews__list")
             ],
             errback = self.errback
         ))    
@@ -19,16 +20,21 @@ class SheinSpider(scrapy.Spider):
     async def parse(self, response):
         page = response.meta['playwright_page']
         await page.close()
-        
-        for link in response.css('a::attr(href)'):
-            yield scrapy.Request(response.urljoin(link.get()), callback=self.parse_link)
 
-    def parse_link(self, response):
-        # Process the individual link here
-        yield {
-            'url': response.url,
-            'title': response.css('title::text').get()
-        }
+        reviews = response.css('div.common-reviews__list')
+        for review in reviews.css('div.j-expose__common-reviews__list-item'):
+            for image in review.css('img.j-review-img::attr(data-src)'):        
+                yield {
+                    'image': image.get()
+                }
+    
+       
+        # Click the "next" button
+        await page.click('span.sui-pagination__next')
+        # Wait for the new reviews to load
+        await page.wait_for_selector("div.common-reviews__list")
+        # Continue parsing the next page
+        await self.parse(response)
     
     async def errback(self, failure):
         page = failure.request.meta['playwright_page']
