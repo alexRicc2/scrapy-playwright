34a8e85864824816bda33afd9313cc3df9fff3a6
alexRicc2 <alex.65.santanna@gmail.com>
2023-07-18 13:20:23 -0300

    saving progresss to test it in windows

--- quotes_js_scraper/spiders/sheinProductsURL.py
+++ quotes_js_scraper/spiders/sheinProductsURL.py
@@ -7,22 +7,17 @@ class SheinProductsURLSpider(CrawlSpider):
     name = 'get_products_urls'
     start_urls = ['https://us.shein.com/']
     allowed_domains = ["br.shein.com", "us.shein.com"]
-    rules = (
-        Rule(LinkExtractor(allow='us.shein.com'), callback='parse_url' , follow=True, process_request='set_playwright_true'),
-    )
-    def set_playwright_true(self, request, response):
-        request.meta["playwright"] = True
-        return request
-
-    def start_requests(self):
-        yield scrapy.Request(
-            url='https://us.shein.com/',
-            meta={"playwright": True}
-        )
-        return super().start_requests()
+    
+    
+    
+    rules = [
+        Rule(LinkExtractor(allow='us.shein.com'), callback='parse_url' , follow=True)
+    ]
  
 
     def parse_url(self, response):
+        # is_product_page = response.css('.product-intro__head-sku')
+        # print('is product page' , is_product_page)
         yield{
             'url': response.url
         }
